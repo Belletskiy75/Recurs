6e0bdc926014ea727d4cc279e85f3d27f589b71a
alex belle <belletskiy75@mail.com>
2024-07-04 18:12:02 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+void PrintArrayRecurs(int[] array, int i)
+{
+    if (i > 0)
+    {
+        Console.Write(array[i - 1] + " ");
+        PrintArrayRecurs(array, i - 1);
+    }
+}
+
+PrintArrayRecurs(array, array.Length);
